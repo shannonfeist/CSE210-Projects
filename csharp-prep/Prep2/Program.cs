3287e3de3b32b7320fc751aca35258a9beb603c7
shannonfeist <shannonfeist@yahoo.com>
2023-11-10 11:09:36 -0600

    Submission

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string letter = "";
+
+        if (percent >= 97)
+        {
+            letter = "A";
+        }
+        else if (percent < 93 && percent >= 90)
+        {
+            letter = "A-";
+        }
+        else if (percent >= 83 && percent < 87)
+        {
+            letter = "B";
+        }
+        else if (percent >= 87 && percent < 90)
+        {
+            letter = "B+";
+        }
+        else if (percent < 83 && percent >= 80)
+        {
+            letter = "B-";
+        }
+        else if (percent >= 73 && percent< 77)
+        {
+            letter = "C";
+        }
+        else if (percent >= 77 && percent < 80)
+        {
+            letter = "C+";
+        }
+        else if (percent < 73 && percent >=70)
+        {
+            letter = "C-";
+        }
+        else if (percent >= 63 && percent < 67)
+        {
+            letter = "D";
+        }
+                else if (percent >= 67 && percent < 70)
+        {
+            letter = "D+";
+        }
+        else if (percent < 63 && percent >= 60)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
+    } 
 }
